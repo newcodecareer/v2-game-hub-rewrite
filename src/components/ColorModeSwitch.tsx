a01adc6348ca432599f793acb92668bad7c51995
newcodecareer <815773677@qq.com>
2026-01-03 11:46:37 +1000

    Implementing color mode

---
+++ src/components/ColorModeSwitch.tsx
@@ -0,0 +1,13 @@
+import { HStack, Switch, Text, useColorMode } from "@chakra-ui/react";
+
+const ColorModeSwitch = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
+  return (
+    <HStack>
+      <Switch onChange={toggleColorMode} isChecked={colorMode === "dark"} />
+      <Text>Color Mode</Text>
+    </HStack>
+  );
+};
+
+export default ColorModeSwitch;
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,11 @@
 import { HStack, Image, Text } from "@chakra-ui/react";
 import logo from "../assets/logo.webp";
+import ColorModeSwitch from "./ColorModeSwitch";
 const NavBar = () => {
   return (
-    <HStack>
-      <Image boxSize='60px' src={logo} />
-      <Text>logo</Text>
+    <HStack justifyContent={"space-between"} padding="10px">
+      <Image boxSize="60px" src={logo} />
+      <ColorModeSwitch />
     </HStack>
   );
 };
