d7bbb93014ae91a63f28345489ba8b94cb13e319
newcodecareer <815773677@qq.com>
2026-01-03 10:42:38 +1000

    Building navbar and get images to assets

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { Button, Grid, GridItem, Show } from "@chakra-ui/react";
 import "./App.css";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <Grid
       templateAreas={{ base: `'nav' 'main'`, lg: `'nav nav' 'aside main'` }}>
       <GridItem area={"nav"} bg={"red.300"}>
-        nav
+        <NavBar/>
       </GridItem>
       <Show above="lg">
         <GridItem area={"aside"} bg={"green.100"}>
